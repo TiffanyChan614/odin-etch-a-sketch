33d7661501ba52248f53e7ebdfbbfae4106bc0e3
TiffChan <tiffanychan1999614@gmail.com>
2022-07-14 14:09:01 +0800

    Add hover effect to cells

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,25 @@
-const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++){
-    let rowContainer = document.createElement("div");
-    rowContainer.classList.add("row-container");
-    for (let j = 0; j < 16; j++){
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        rowContainer.appendChild(cell);
+function createGrid(){
+    const container = document.querySelector(".container");
+    for (let i = 0; i < 16; i++){
+        let rowContainer = document.createElement("div");
+        rowContainer.classList.add("row-container");
+        for (let j = 0; j < 16; j++){
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            rowContainer.appendChild(cell);
+        }
+        container.appendChild(rowContainer);
     }
-    container.appendChild(rowContainer);
 }
+
+function changeColorWhenHover(cells){
+    Array.from(cells).forEach(cell => {
+        cell.addEventListener('mouseover', (event) => {event.target.style.backgroundColor = "black"});
+        cell.addEventListener('mouseout', (event) => {event.target.style.backgroundColor = "grey"});
+    });
+}
+
+createGrid()
+
+const cells = document.querySelectorAll(".cell");
+changeColorWhenHover(cells);
