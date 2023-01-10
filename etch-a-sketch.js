92b91d8b11855b15967741475d8563a1690639ee
TiffChan <tiffanychan1999614@gmail.com>
2023-01-10 18:36:44 -0500

    Fix: add data validation in size input

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,82 +1,86 @@
 const canvas = document.querySelector(".canvas");
 
-function clearGrid(){
-    canvas.textContent = "";
+function clearGrid() {
+  canvas.textContent = "";
 }
 
-function createGrid(size){
-    canvas.style.width = "600px";
-    canvas.style.height = "600px";
-    for (let i = 0; i < size; i++){
-        let rowContainer = document.createElement("div");
-        rowContainer.classList.add("row-container");
-        for (let j = 0; j < size; j++){
-            let cell = document.createElement("div");
-            cell.classList.add("cell", "black");
-            rowContainer.appendChild(cell);
-        }
-        canvas.appendChild(rowContainer);
+function createGrid(size) {
+  canvas.style.width = "600px";
+  canvas.style.height = "600px";
+  for (let i = 0; i < size; i++) {
+    let rowContainer = document.createElement("div");
+    rowContainer.classList.add("row-container");
+    for (let j = 0; j < size; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell", "black");
+      rowContainer.appendChild(cell);
     }
+    canvas.appendChild(rowContainer);
+  }
 }
 
-function changeColorWhenHover(){
-    const blackCells = document.querySelectorAll(".black", ".cell");
-    const rainbowCells = document.querySelectorAll(".rainbow", ".cell");
-    Array.from(blackCells).forEach(cell => {
-        cell.addEventListener('mouseover', (event) => useBlack(event));
-    });
-    Array.from(rainbowCells).forEach(cell => {
-        cell.addEventListener('mouseover', (event) => useRainbow(event));
-    });
+function changeColorWhenHover() {
+  const blackCells = document.querySelectorAll(".black", ".cell");
+  const rainbowCells = document.querySelectorAll(".rainbow", ".cell");
+  Array.from(blackCells).forEach((cell) => {
+    cell.addEventListener("mouseover", (event) => useBlack(event));
+  });
+  Array.from(rainbowCells).forEach((cell) => {
+    cell.addEventListener("mouseover", (event) => useRainbow(event));
+  });
 }
 
-function useBlack(event){
-    event.target.style.backgroundColor = "black";
+function useBlack(event) {
+  event.target.style.backgroundColor = "black";
 }
 
-function useRainbow(event){
-   event.target.style.backgroundColor = generateRandomColor();
+function useRainbow(event) {
+  event.target.style.backgroundColor = generateRandomColor();
 }
 
-function turnBlack(){
-    const cells = document.querySelectorAll(".cell");
-    Array.from(cells).forEach((cell) => cell.classList.add("black"));
-    Array.from(cells).forEach((cell) => cell.classList.remove("rainbow"));
-    changeColorWhenHover();
+function turnBlack() {
+  const cells = document.querySelectorAll(".cell");
+  Array.from(cells).forEach((cell) => cell.classList.add("black"));
+  Array.from(cells).forEach((cell) => cell.classList.remove("rainbow"));
+  changeColorWhenHover();
 }
 
-function turnRainbow(){
-    const cells = document.querySelectorAll(".cell");
-    Array.from(cells).forEach((cell) => cell.classList.add("rainbow"));
-    Array.from(cells).forEach((cell) => cell.classList.remove("black"));
-    changeColorWhenHover();
+function turnRainbow() {
+  const cells = document.querySelectorAll(".cell");
+  Array.from(cells).forEach((cell) => cell.classList.add("rainbow"));
+  Array.from(cells).forEach((cell) => cell.classList.remove("black"));
+  changeColorWhenHover();
 }
 
-function promptGridSize(){
-    let size = prompt("Enter new size: ", "");
-    while(!size){
-        alert("Size cannot be empty");
-        size = prompt("Enter new size: ", "");
+function promptGridSize() {
+  let validSize;
+  do {
+    size = prompt("Enter new size: ", "");
+    validSize = size > 0 && size <= 200;
+    if (!validSize) {
+      alert("Size must be between 1 and 200");
+    } else if (!size) {
+      alert("Size cannot be empty");
     }
-    clearGrid();
-    run(size);
+  } while (!size || !validSize);
+  clearGrid();
+  run(size);
 }
 
-function generateRandomColor(){
-    let res = "rgb(";
-    let colors = [];
-    for (let i = 0; i < 3; i++){
-        colors.push(Math.floor(Math.random() * 256));
-        res += colors[i];
-        if (i < 2) res += ","
-    }
-    return res + ")";
+function generateRandomColor() {
+  let res = "rgb(";
+  let colors = [];
+  for (let i = 0; i < 3; i++) {
+    colors.push(Math.floor(Math.random() * 256));
+    res += colors[i];
+    if (i < 2) res += ",";
+  }
+  return res + ")";
 }
 
-function run(size){
-    createGrid(size);
-
-    changeColorWhenHover();
+function run(size) {
+  createGrid(size);
+  changeColorWhenHover();
 }
 
 let size = 16;
